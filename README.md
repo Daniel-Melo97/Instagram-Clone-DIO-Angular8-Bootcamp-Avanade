1c1778ac5c48c4733ba3d2fae6d22f82711df741
Daniel-Melo97 <daniellemosmelo@hotmail.com>
2021-03-03 17:54:15 -0300

    Subindo Projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+# instragram clone feito com a DIO para o bootcamp Angular
+
+Nesse Projeto eu apenas refiz o código, adicionando elementos novos na página, alterando elementos já existentes e também
+mudando o estilo da página de login do Instagram.
+
+Tomei liberdade também de adicionar funções bem simples aos botões com javascript.
---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function abrir_applestore(){
+    window.open('https://apps.apple.com/app/instagram/id389801252?vt=lo')
+}
+
+function abrir_googleplay(){
+    window.open('https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DA1A81CC9-EBFA-4A63-9D30-46882848F7D9%26utm_content%3Dlo%26utm_medium%3Dbadge')
+}
+
+function abrir_facebook(){
+    window.open('https://web.facebook.com/');
+}
+
+function login(event){
+    event.preventDefault();
+    alert('Obrigado por ver minha aplicação!');
+}
